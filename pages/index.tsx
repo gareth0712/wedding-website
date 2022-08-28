73e0ea7f18e684901b6f88c94d915a61b69f9d3d
gareth0712 <gareth_0712@hotmail.com>
2022-08-28 22:13:37 +0800

    Add files for website including workflows

---
+++ pages/index.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  useEffect(() => {
+    const docUrl: string =
+      'https://docs.google.com/document/d/12aAKcSGAwlmyu6k_1xBdqgVLR1pllY0z_yKL1Og63i0/edit?usp=sharing';
+    window.location.replace(docUrl);
+  });
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Fung and Lydia's Wedding</title>
+        <meta name='description' content='Created by Garen Studio' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className={styles.main}>網站建設中, 將自動轉至請柬頁...</main>
+
+      <footer className={styles.footer}>
+        <a
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Powered by Garen Studio
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
